ac3225251bbdb26abd10371344bc20d4353f724d
mohammedsuhail85 <suhailmsm@gmail.com>
2018-09-25 23:11:41 +0530

    First Commit

---
+++ CSSE-WE19/Procurement Department/src/lk/sliit/csse/group19/ProcurementDepartment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lk.sliit.csse.group19.gui;
+
+/**
+ *
+ * @author Muhammed Suhail
+ */
+public class ProcurementDepartment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+    
+}
